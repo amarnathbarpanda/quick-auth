cb93323a6f98aa62c56e0ef1638ac706dd90eb12
Amarnath Barpanda <47183341+amarnathbarpanda@users.noreply.github.com>
2022-11-10 02:12:22 +0530

    Add connect-mongo to store the session, flash

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ const csurf = require('csurf');
 const express = require('express');
 const app = express();
 const expressSession = require('express-session')
+const cookieParser = require('cookie-parser');
+const MongoStore = require('connect-mongo'); 
 
+const passport = require('passport');
+const flash = require('connect-flash');
 const db = require('./config/mongoose');
 
 const port  = process.env.PORT || 8000;
@@ -23,11 +27,38 @@ app.set('views', './views');
 // settiing up body parser
 app.use(express.urlencoded({extended: true}));
 
+app.use(cookieParser('amar2324'))
+
 app.use(expressSession({
-    secret: 'amar2324'
+    name: 'quick-auth',
+    secret: 'amar2324',
+    resave: true, 
+    // this forces the sessions to be saved back to our store
+    saveUninitialized: false,
+    cookie: {
+        maxAge: (1000 * 60 * 100)
+    },
+    store: new MongoStore({
+        // mongooseConnection: db, this won't work 
+        mongoUrl: 'mongodb://localhost/quick-auth',
+        autoRemove: 'disabled'
+    },
+        function (err) {
+            console.log(err || 'connect-mongo ok');
+        })
 }));
 
 app.use(csurf());
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(flash());
+
+app.use((req, res, next)=>{
+    res.locals.error = req.flash('error');
+    next();
+});
 
 app.use('/', require('./routes'));
 
